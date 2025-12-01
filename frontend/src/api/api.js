c0e8bad309f0cf684d9cfcbcccd6b6201cf654d0
101485895 <101485895@georgebrown.ca>
2025-11-30 21:41:06 -0500

    attempt again to fix login

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const backendURL =
+  process.env.REACT_APP_BACKEND_URL || "http://localhost:8081/api/v1";
+
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL
+  baseURL: backendURL
 });
